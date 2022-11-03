bf4b280eb2c722378b6660de634a7d62912d190f
gabriel <gabrielbvlima@gmail.com>
2022-11-02 22:05:29 -0300

    Adição do primeiro projeto

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -1,4 +1,8 @@
+import { projectsData } from "../dataBase/projects";
 import "../styles/components/projectscontainer.sass";
+import "../styles/components/project.sass";
+
+const projects = projectsData;
 
 const ProjectsContainer = () => {
     return (
@@ -12,12 +16,30 @@ const ProjectsContainer = () => {
                 Projetos futuros estarão expostos aqui no portfolio para que vocês possam acompanhar
                 minha evolução.
             </p>
-            <div className="buttons">
-                <a href="" className="btn_projects">
-                    Ver Projetos
-                </a>
+
+            <div className="project-grid">
+                {projects.map((project) => (
+                    <div className="project-card" id={project.id} key={project.id}>
+                        <img src={project.img} alt={project.name} />
+                        <div className="project-info">
+                            <h3>{project.name}</h3>
+                            <p>{project.description}</p>
+                        </div>
+                        <div className="project-links">
+                            <a className="btn-project" href={project.repository} target="__blank">
+                                Repositorio
+                            </a>
+                            <a className="btn-project" href={project.link} target="__blank">
+                                Aplicação
+                            </a>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            <div className="button-portfolio">
                 <a
-                    className="btn_projects"
+                    className="btn-portfolio"
                     href="https://portfolio-gabriel-gilt.vercel.app/"
                     target="__blank"
                 >
--- src/components/TechnologiesContainer.jsx
+++ src/components/TechnologiesContainer.jsx
@@ -1,20 +1,7 @@
-import { DiHtml5, DiCss3, DiJsBadge, DiSass, DiNodejsSmall, DiReact } from "react-icons/di";
-
+import { technologiesData } from "../DataBase/technologies";
 import "../styles/components/technologiescontainer.sass";
 
-const technologies = [
-    { id: "html", name: "HTML5", icon: <DiHtml5 />, level: "Mais de 1 ano de experiência." },
-    { id: "css", name: "CSS3", icon: <DiCss3 />, level: "Mais de 1 ano de experiência." },
-    { id: "sass", name: "Sass", icon: <DiSass />, level: "Entre 0 e 6 meses de experiência" },
-    { id: "js", name: "JavaScript", icon: <DiJsBadge />, level: "Mais de 1 ano de experiência." },
-    { id: "react", name: "React", icon: <DiReact />, level: "Mais de 1 ano de experiência." },
-    {
-        id: "node",
-        name: "Node.js",
-        icon: <DiNodejsSmall />,
-        level: "Mais de 1 ano de experiência.",
-    },
-];
+const technologies = technologiesData;
 
 const TechnologiesContainer = () => {
     return (
