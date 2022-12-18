c8d0c47ae4581fa7ffd585706af6da315a255f18
Gabriel <gabrielbvlima@gmail.com>
2022-12-17 21:50:26 -0300

    Teste

--- src/dataBase/technologies.jsx
+++ src/dataBase/technologies.jsx
@@ -3,7 +3,7 @@ import { DiHtml5, DiCss3, DiJsBadge, DiSass, DiNodejsSmall, DiReact } from "reac
 export const technologiesData = [
     { id: "html", name: "HTML5", icon: <DiHtml5 />, level: "Mais de 1 ano de experiência." },
     { id: "css", name: "CSS3", icon: <DiCss3 />, level: "Mais de 1 ano de experiência." },
-    { id: "sass", name: "Sass", icon: <DiSass />, level: "Entre 0 e 6 meses de experiência" },
+    { id: "sass", name: "Sass", icon: <DiSass />, level: "Mais de 1 ano de experiência." },
     { id: "js", name: "JavaScript", icon: <DiJsBadge />, level: "Mais de 1 ano de experiência." },
     { id: "react", name: "React", icon: <DiReact />, level: "Mais de 1 ano de experiência." },
     {
